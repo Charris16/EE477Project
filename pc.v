1b2c61927b00fd2d437f2a8feb5348a0e4e5e839
Charris16 <cadesharris@gmail.com>
2022-03-15 13:52:58 -0700

    rewrote program counter

--- pc.v
+++ pc.v
@@ -1,4 +1,13 @@
-module pc(IP, PC_def, up_amt, b_taken, OP, CLK, RESET);
+module pc(
+    IP,
+    PC_def,
+    up_amt,
+    b_taken,
+    OP,
+    CLK,
+    RESET
+    );
+
     input logic [31:0] up_amt;
     input logic [6:0] OP;
     input logic b_taken, CLK, RESET;
@@ -6,8 +15,7 @@ module pc(IP, PC_def, up_amt, b_taken, OP, CLK, RESET);
 
     assign PC_def = IP + 32'd4;
     
-    enum {INC4, STALL, JUMP} ps, ns;
-    logic [31:0] inc_amt;
+    enum {INC4, STALL} ps, ns;
     always_comb begin
         case(ps)
             INC4: begin
@@ -17,29 +25,21 @@ module pc(IP, PC_def, up_amt, b_taken, OP, CLK, RESET);
                 else ns = INC4;
             end
             STALL: begin
-                if(b_taken) ns = JUMP;
-                else if (OP == 7'b1101111) ns = JUMP;
-                else if (OP == 7'b1100111) ns = JUMP;
-                else ns = INC4;
+                ns = INC4;
             end
-            JUMP: ns = INC4;
-            default ns = INC4;
-        endcase
-    end 
-
-    always_comb begin
-        case(ns)
-            INC4: inc_amt = 32'd4;
-            STALL: inc_amt = 32'b0;
-            JUMP: inc_amt = up_amt;
-            default: inc_amt = 32'd4;
         endcase
     end
 
     always_ff @(posedge CLK) begin
-        if(RESET) IP <= 32'b0;
-        else IP <= IP + inc_amt;
+        IP <= 32'b0;
+        case(ps)
+            INC4: IP <= IP + 32'd4;
+            STALL: IP <= b_taken ? IP + up_amt : IP + 32'd4;
+        endcase
     end
+
+
+
 
     always_ff @(posedge CLK) begin
         if(RESET) ps <= INC4;
