ea682c7adb2b8bbc5b6d6c8da887ff9dcf4f8cc8
Charris16 <cadesharris@gmail.com>
2022-03-15 14:30:55 -0700

    Furtuer restructing of pc

--- pc.v
+++ pc.v
@@ -13,10 +13,12 @@ module pc(
     input logic b_taken, CLK, RESET;
     output logic [31:0] IP, PC_def;
 
-    assign PC_def = IP + 32'd4;
     
+    
+    logic [31:0] inc_amt;
     logic stall_pc;
     assign stall_pc = (OP == 7'b1101111) | (OP == 7'b1100111) | (OP == 7'b1100011);
+    assign PC_def = IP + 32'd4;
 
     enum {INC4, STALL} ps, ns;
     always_comb begin
@@ -32,26 +34,23 @@ module pc(
         endcase
     end
 
-    always_ff @(posedge CLK) begin
-        IP <= 32'b0;
+    always_comb begin
         case(ps)
-            INC4: begin
-                if(RESET) IP <= 32'b0;
-                else IP <= stall_pc ? IP : IP + 32'd4;
-            end
-            STALL: IP <= b_taken ? IP + up_amt : IP + 32'd4;
+            INC4: inc_amt = stall_pc ? IP : IP + 32'd4;
+            STALL: inc_amt = b_taken ? IP + up_amt : IP + 32'd4;
+            default inc_amt = IP + 32'd4;
         endcase
     end
-
-
-
 
     always_ff @(posedge CLK) begin
         if(RESET) begin
             ps <= INC4;
             IP <= 32'b0;
         end
-        else ps <= ns;
+        else begin 
+            ps <= ns;
+            IP <= inc_amt;
+        end
     end
 endmodule
 
