92fe8112e628a60f3916053b989795e2ad12db62
Charris16 <cadesharris@gmail.com>
2022-03-18 14:11:36 -0700

    needed another command

--- sim.py
+++ sim.py
@@ -1,6 +1,7 @@
 #!/bin/tcsh
 echo $0
 setenv EDA_TOOLS_PATH /home/lab.apps/vlsiapps_new/
+setenv USE_CALIBRE_VCO aoj
 set path = (/home/lab.apps/vlsiapps_new/icc2/current/bin $path)
 source /home/lab.apps/vlsiapps_new/cshrc/general.cshrc
 python3 runsim.py
