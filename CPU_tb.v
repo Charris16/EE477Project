b0ef04c89b436df4d225800ac22ded5c9d067bf0
Charris16 <cadesharris@gmail.com>
2022-03-18 15:55:12 -0700

    Adding apr makefile

--- CPU_tb.v
+++ CPU_tb.v
@@ -1,6 +1,36 @@
 module CPU_tb();
     logic CLK, Reset;
-    CPU dut(CLK, Reset);
+
+
+    logic [31:0] Instr_Addr, instruction, MEM_data, MEM_WR_out, MEM_addr;
+    logic [2:0] MEM_type;
+    logic MEM_rd_en, MEM_wr_en;
+    instructmem INSTRUC_MEM(
+        .address(Instr_Addr),
+        .instruction(instruction),
+        .clk(CLK)
+        );
+    CPU_TopLevel CPU_TopLevel0(
+        .Instr_Addr(Instr_Addr),
+        .MEM_addr(MEM_addr),
+        .MEM_WR_out(MEM_WR_out),
+        .MEM_type(MEM_type),
+        .MEM_rd_en(MEM_rd_en),
+        .MEM_wr_en(MEM_wr_en),
+        .INSTRUCTION(instruction),
+        .MEM_data(MEM_data),
+        .CLK(CLK),
+        .Reset(Reset)
+        );
+     Memory DATA_MEM(
+        .address(MEM_addr),
+        .write_enable(MEM_wr_en),
+        .read_enable(MEM_rd_en),
+        .write_data(MEM_WR_out),
+        .clk(CLK),
+        .xfer_size(MEM_type),
+        .read_data(MEM_data)
+	    );
     parameter clock_period = 10;
     initial begin
         CLK <=1'b0;
@@ -8,6 +38,7 @@ module CPU_tb();
     end
 
     initial begin
+//	$sdf_annotate("./CPU_TopLevel.syn.sdf", CPU_TopLevel0);
         $vcdpluson;
         $dumpvars();
 	    Reset <= 1'b1; @(posedge CLK);
---
+++ apr/merge.sh
@@ -0,0 +1,1 @@
+calibredrv -a layout filemerge -in CPU_TopLevel.gds -indir /home/lab.apps/vlsiapps/kits/tsmc/N65RF/1.0c/digital/Back_End/gds/tcbn65gplus_140a -out CPU_TopLevel.apr.merge.gds -topcell CPU_TopLevel
