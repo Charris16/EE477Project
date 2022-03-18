237b7798d73649caaf9af2b10e33fce937d0e829
Charris16 <cadesharris@gmail.com>
2022-03-18 14:10:04 -0700

    Changing script to work hopfully

--- sim.py
+++ sim.py
@@ -1,3 +1,6 @@
 #!/bin/tcsh
 echo $0
+setenv EDA_TOOLS_PATH /home/lab.apps/vlsiapps_new/
+set path = (/home/lab.apps/vlsiapps_new/icc2/current/bin $path)
+source /home/lab.apps/vlsiapps_new/cshrc/general.cshrc
 python3 runsim.py
